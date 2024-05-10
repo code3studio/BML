58028f5d16f6fbbd09653d5dcf3b96af68e2b238
sparqxi <zhongxi1992@gmail.com>
2024-05-10 02:54:06 -0700

    loading total loading

--- frontend/src/page/token-page/TokenPage.tsx
+++ frontend/src/page/token-page/TokenPage.tsx
@@ -1,18 +1,22 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Skeleton, Typography } from "@mui/material";
 import TokenInfoContent from "./token-info-content/TokenInfoContent";
 import { useEffect, useState } from "react";
 import { getCounts } from "../../services/api";
 
 const TokenPage = () => {
   const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         let result = await getCounts();
         console.log("resu==", result.data);
         setTotal(result.data);
+        setLoading(false);
       } catch (error) {
         console.error(error);
+        setLoading(false);
       }
     };
     fetchData();
@@ -26,7 +30,12 @@ const TokenPage = () => {
       </Grid>
       <Grid container justifyContent={"center"}>
         <Typography variant="h5" mt={1} textAlign={"center"}>
-          {total} Tokens Created
+          {loading ? (
+            <Skeleton sx={{ display: "inline-block" }} width={40} />
+          ) : (
+            total
+          )}{" "}
+          Tokens Created
         </Typography>
       </Grid>
 
